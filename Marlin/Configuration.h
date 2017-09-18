167183447cb09812acaaf7fa7437355ee2dcf380
Farzad Battiwalla <farzadb82@gmail.com>
2017-09-17 21:46:36 -0400

    Calibrated E steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -383,7 +383,7 @@
 // or to allow moving the extruder regardless of the hotend temperature.
 // *** IT IS HIGHLY RECOMMENDED TO LEAVE THIS OPTION ENABLED! ***
 #define PREVENT_COLD_EXTRUSION
-#define EXTRUDE_MINTEMP 170
+#define EXTRUDE_MINTEMP 185
 
 // This option prevents a single extrusion longer than EXTRUDE_MAXLENGTH.
 // Note that for Bowden Extruders a too-small value here may prevent loading.
@@ -492,7 +492,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 96 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 100 }
 
 /**
  * Default Max Feed Rate (mm/s)
