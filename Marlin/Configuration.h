cb57fe233fb37125998e98c0873203733b7724f7
Farzad Battiwalla <farzadb82@gmail.com>
2017-10-22 23:58:34 -0400

    Increased xy homing feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -998,7 +998,7 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (50*60)
+#define HOMING_FEEDRATE_XY 8000
 #define HOMING_FEEDRATE_Z  (4*60)
 
 //=============================================================================
