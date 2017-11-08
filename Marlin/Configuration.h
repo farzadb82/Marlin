8c1863a76f8af425e6fd3a4f2bb659e539deec8e
Farzad Battiwalla <farzadb82@gmail.com>
2017-11-07 22:33:15 -0500

    Updated default Z steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -534,7 +534,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 100 }
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 100 }  // T8, 2 start
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 100 }   // T8, 4 start
 
 /**
  * Default Max Feed Rate (mm/s)
